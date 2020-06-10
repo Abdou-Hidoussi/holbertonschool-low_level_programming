72cd6132de0d56951b6b81a22be7b28909520367
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-10 12:15:01 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-	system(1 ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 54);
+	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	while (*s) putchar(*s++);
 	return (1);
 	}
