5f16ffd4d487fb1424ffc9a494add6df17d3522c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-10 09:40:08 +0100

    first commit of the day

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - puts string
+*"betty you're a cunt"
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
