557d2b3de22f0d0aef79d85fd675123015514fc2
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-11 14:48:20 +0100

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		for (j = i; j <=  '9'; ++j)
+		{
+			for (z = j; z <=  '9'; ++j)
+			{
+				if (i != j)
+				{
+					if (i != '0' || j != '1' || z != '2')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(i);
+					putchar(j);
+					putchar(z);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
