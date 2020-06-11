27de1398cf0d11e9e7a2b7e328d6ffccd75425fc
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-11 12:40:06 +0100

    trr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,19 +11,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n)
-	{
+	
 		if (n > 0)
 		{
 				printf("%d is positive\n", n);
 		}
-		else
+		else if (n < 0)
 		{
 				printf("%d is negative\n", n);
 		}
-		return (0);
-	}
-	printf("%d is zero\n", n);
+		else
+		{
+			printf("%d is zero\n", n);
+		}	
 	return (0);
 }
