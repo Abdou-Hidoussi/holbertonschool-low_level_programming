2aff461e6bc20fd108272132987107bb028c929c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 03:37:19 +0100

    go

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,6 +19,7 @@ void times_table(void)
 					{
 						_putchar(',');
 						_putchar(' ');
+						_putchar(' ');
 					}
 			}
 			else
@@ -27,6 +28,7 @@ void times_table(void)
 					if (j != 9)
 					{
 						_putchar(',');
+						_putchar(' ');
 						_putchar(' ');
 					}
 			}
