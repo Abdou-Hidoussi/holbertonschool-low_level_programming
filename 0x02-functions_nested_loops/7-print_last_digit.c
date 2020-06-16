54bbf0fee644fd51eaf24b10e8e253936980cff5
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 10:01:10 +0100

    betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,11 @@ int print_last_digit(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n >= 0)
 	{
 		i = n % 10;
 		_putchar(i + '0');
 		return (i);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
 	}
 	else
 	{
