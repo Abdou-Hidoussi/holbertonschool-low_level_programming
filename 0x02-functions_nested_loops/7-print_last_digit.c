d38813124d4e8a9d30c6c36a34d4e886d96cb066
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 05:07:30 +0100

    finally

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,24 +3,16 @@
 *print_alphabet_x10 - 0
 *Return: nothing
 */
-
-int _abs(int n)
-{
-	if (n >= 0)
-	{
-		return (n);
-	}
-
-	return (n * (-1));
-}
-
-
 int print_last_digit(int n)
 {
-	int i;
-
-	i = _abs(n);
-	i %= 10;
-	_putchar('0' + i);
-	return (i);
+	if (n > 0)
+	{
+		_putchar((n % 10) + '0');
+		return (n % 10);
+	}
+	else
+	{
+		_putchar(((n * (-1)) % 10) + '0');
+		return ((n * (-1)) % 10);
+	}
 }
