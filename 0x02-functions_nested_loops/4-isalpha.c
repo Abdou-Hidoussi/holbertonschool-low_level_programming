d584ebf800cd038d57990d04c237ae3c4a422252
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 03:08:56 +0100

    go

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
-int _isalpha(int c){
-	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+#include "holberton.h"
+/*
+*print_alphabet_x10 - 0
+*Return: nothing
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
