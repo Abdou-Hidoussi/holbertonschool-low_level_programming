d4716106e60cde375006c20e142695663b3770c3
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 18:11:40 +0100

    go

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,20 +6,20 @@
 */
 int main(void)
 {
-	long int i, j, z, t, r;
+	long i, j, z, t, r;
 
 	z = 1;
 	j = 1;
 	t = z;
 	i = 3;
-	while (i <= 4000000)
+	while (j <= 4000000)
 	{
 		j = j + t;
 		t = z;
 		z = j;
 		if (t % 2 != 0)
 		{
-			r += t;
+			r += j;
 		}
 		i++;
 	}
