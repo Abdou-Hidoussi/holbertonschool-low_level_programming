5f8645c2b8573e03b7538eb198d6ab6451b320b3
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 04:01:42 +0100

    just no

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include "_putchar.c"
 /*
 *print_alphabet_x10 - 0
 *Return: nothing
@@ -14,31 +13,26 @@ void times_table(void)
 		{
 			if (i * j > 9)
 			{
-					if (j != 9 && j!= 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+				if (j != 9 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
 			}
 			else
 			{
-					if (j != 9 && j!= 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
+				if (j != 9 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar((i * j) + '0');
 			}
 
 		}
 		_putchar('\n');
 	}
-}
-int main(void)
-{
-    times_table();
-    return (0);
 }
