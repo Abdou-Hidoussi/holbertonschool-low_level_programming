3bcba581ef582fcc3492f2b4aed1ceb680113be4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 05:23:55 +0100

    finally

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,45 @@
-void print_times_table(int n){
+#include "holberton.h"
+/*
+*print_alphabet_x10 - 0
+*Return: nothing
+*/
+void table(int n)
+{
+	if (i * j > 99)
+	{
+		_putchar((i * j) / 100 + '0');
+		_putchar(((i * j) / 10) % 10 + '0');
+		_putchar((i * j) % 10 + '0');
+		if (j != n)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+	}
+	else if (i * j > 9)
+	{
+		_putchar((i * j) / 10 + '0');
+		_putchar((i * j) % 10 + '0');
+		if (j != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		_putchar((i * j) + '0');
+		if (j != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
+
+
+void print_times_table(int n)
+{
 	int i, j;
 
 	if (n < 15)
@@ -7,40 +48,10 @@ void print_times_table(int n){
 		{
 			for (j = 0; j <= n; ++j)
 			{
-				if (i*j > 99)
-				{
-					_putchar((i * j )/ 100 + '0');
-					_putchar(((i * j )/ 10 )% 10 + '0');
-					_putchar((i * j )% 10 + '0');
-						if (j != n)
-						{
-							_putchar(',');
-							_putchar(' ');
-						}					
-				}
-				else if (i*j > 9)
-				{
-					_putchar((i * j )/ 10 + '0');
-					_putchar((i * j )% 10 + '0');
-						if (j != n)
-						{
-							_putchar(',');
-							_putchar(' ');
-						}
-				}
-				else
-				{
-					_putchar((i * j )+'0');
-						if (j != n)
-						{
-							_putchar(',');
-							_putchar(' ');
-						}				
-				}
-
+				table(n);
 			}
 			_putchar('\n');
-		}		
+		}
 	}
 
 }
