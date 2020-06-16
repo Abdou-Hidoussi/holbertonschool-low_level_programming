e4cc8e895d45aa305d3481b5009b6a3665b69922
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-16 17:47:02 +0100

    go

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,16 @@
 */
 void main(void)
 {
-	int i, j;
+	int i;
+	long int j;
 
 	j = 0;
-	for (int i = 0; i < 1024; ++i)
+	for (i = 0; i < 1024; ++i)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			j += i;
 		}
 	}
-	printf("%d\n", j);
+	printf("%ld\n", j);
 }
