d9357513acbb12aa06335d9ffecf1e121077ea1b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-18 19:41:13 +0100

    test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,15 +9,32 @@ void print_number(int n)
 	int i, r;
 
 	i = n;
-	r = 0;	
-	while(i > 0)
+	r = 0;
+	if (n > 0)
 	{
-		r = (r*10) + (i%10);
-		i = i / 10;
+		i = n;
 	}
-	while (r > 0)
+	else if (n < 0)
 	{
-		_putchar(r % 10 + '0');
-		r = r / 10;
+		i = -n;
+		_putchar('-');
 	}
+	if (n != 0)
+	{
+		while (i > 0)
+		{
+			r = (r * 10) + (i % 10);
+			i = i / 10;
+		}
+		while (r > 0)
+		{
+			_putchar(r % 10 + '0');
+			r = r / 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+	}
+
 }
