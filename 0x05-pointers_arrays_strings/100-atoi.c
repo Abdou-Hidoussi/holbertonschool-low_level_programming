823cd6cf691d15a355bc027dc17e7c3ab7ea874d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-23 05:28:40 +0100

    change return case 0

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 /**
 *_atoi - convert string to int
 *@s: input string
@@ -62,5 +61,37 @@ int _atoi(char *s)
 		}
 		s++;
 	}
-	return (num * signe);
+	return (0);
 }
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _atoi("-+---+--5-+++Battery Street; San 5Francisco,");
+    printf("%d\n", n);
+    n = _atoi("-402");
+    printf("%d\n", n);
+    n = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", n);
+    n = _atoi("214748364");
+    printf("%d\n", n);
+    n = _atoi("0");
+    printf("%d\n", n);
+    n = _atoi("Suite 402");
+    printf("%d\n", n);
+    n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", n);
+    n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", n);
+    return (0);
+}
+
