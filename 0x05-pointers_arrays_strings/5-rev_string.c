184668f37e7d44f6b2eba6bf9b4c8c4d88644f7e
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-23 10:50:58 +0100

    misery

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*rev_string - 0
+*@s: string
+*
+*/
+void rev_string(char *s)
+{
+	char c;
+	int i, j;
+
+	i = 0;
+	while (s[i + 1] != '\0')
+	{
+		i++;
+	}
+
+	j = i;
+	i = 0;
+	while (i < j / 2 + 1)
+	{
+		c = s[i];
+		s[i] = s[j - i];
+		s[j - i] = c;
+		i++;
+		printf("%s\n", s);
+	}
+}
