aea01b066ac8e854fcab38990f2472b7d56e428d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-23 09:02:50 +0100

    test

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*print_rev - 0
+*@s: char
+*
+*/
+void print_rev(char *s)
+{
+	char f;
+
+	f = *s;
+	while (*s != '\0')
+	{
+		s++;
+	}
+
+	while (*s != f)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
