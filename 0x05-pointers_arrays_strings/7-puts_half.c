65dfea5aa0c3fc440dead34c220e1ef0702adeeb
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-23 11:12:41 +0100

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*puts_half - 0
+*@str: char
+*
+*/
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	j = i;
+	i = j / 2;
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
