2dd6e25c2f3b0502f6e5cc62454366f68471130e
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-25 13:25:12 +0100

    es

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+*_strcmp - 0
+*@s1: string
+*@s2: string
+*Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int n1, n2, i;
+
+	n1 = 0;
+	n2 = 0;
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		n1 += s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		n2 += s1[i];
+		i++;
+	}
+
+	if (n1 > n2)
+	{
+		return (15);
+	}
+	if (n2 > n1)
+	{
+		return (-15);
+	}
+	return (0);
+}
