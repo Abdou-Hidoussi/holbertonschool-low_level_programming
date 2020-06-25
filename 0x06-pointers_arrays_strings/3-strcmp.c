cbbf3a09e7200042ce6c16b709485784d6e189b4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-25 17:56:43 +0100

    tets

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,33 +7,19 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int n1, n2, i;
+	int j, i;
 
-	n1 = 0;
-	n2 = 0;
 	i = 0;
+	j = 0;
 	while (s1[i] != '\0')
 	{
-		n1 += s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		n2 += s2[i];
 		i++;
 	}
 
-	if (n1 > n2)
+	while (s1[j] == s2[j] && j < i)
 	{
-		return (15);
+		j++;
 	}
-	else if (n1 < n2)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (s1[j] - s2[j]);
 }
