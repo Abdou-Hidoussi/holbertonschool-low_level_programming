ab17e7c9d40557471773b69a14345bc831d564ff
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-25 20:32:14 +0100

    a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*printRandoms - 0
+*@lower: int
+*@upper: int
+*/
+void printRandoms(int lower, int upper)
+{
+	int sum, i;
+	char c;
+
+	sum = 0;
+	c = (rand() % (upper - lower + 1)) + lower;
+	putchar(c);
+
+	sum += c;
+	while (sum < 2772 - 123)
+	{
+		c = (rand() % (upper - lower + 1)) + lower;
+		sum += c;
+		putchar(c);
+	}
+
+	while (sum + c != 2772)
+	{
+		c++;
+	}
+	sum += c;
+	putchar(c);
+}
 /**
 *main - 0
 *Return: 0
 */
 int main(void)
 {
-	printf("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+	int lower = 'A', upper = 'Z';
+
+	srand(time(0));
+
+	printRandoms(lower, upper);
+
 	return (0);
 }
