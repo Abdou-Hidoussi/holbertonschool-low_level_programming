df397b19cf5310684d893fdba59b60c4fd325713
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-25 13:13:09 +0100

    es

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	if (n > 0)
+
+	for (i = 0; i < n && src[i] != '\0'; ++i)
 	{
-		for (i = 0; i < n; ++i)
-		{
-			if (src[i] != '\0')
-			{
-				dest[i] = src[i];
-			}
-			else
-			{
-				break;
-			}
-		}		
+		dest[i] = src[i];
 	}
+
+	while (i < n)
+	{
+		i++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
