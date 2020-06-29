efc2ab3c00b8fe62dd98d2bd37b30a843ce4cc3f
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-29 21:19:56 +0100

    h

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,10 @@ unsigned int _strspn(char *s, char *accept)
 
 	n = 0;
 	j = 0;
-	while (s[j] != '\0')
+	while (s[j] != '\0' && s[j] != ' ')
 	{
 		i = 0;
-		while (s[i] == accept[i])
+		while (accept[i] != '\0')
 		{
 			if (accept[i] == s[j])
 			{
