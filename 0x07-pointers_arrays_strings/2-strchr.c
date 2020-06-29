34def81668e2329e041ab16fd8ef89f9019fb8e6
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-29 22:25:06 +0100

    f

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,7 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	if (s[0] == '\0')
-	{
-		return (0);
-	}
-	if (c == '\0')
-	{
-		i = 0;
-		while (s[i] != '\0')
-		{
-			i++;
-		}
-		return (&s[i + 1]);
-	}
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
