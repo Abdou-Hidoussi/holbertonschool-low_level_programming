292202d937955784e066be3a25c1e856e26d52c5
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-29 12:05:38 +0100

    first

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*_strspn - 0
+*@s: string
+*@accept: string
+*Return: unsigned int
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] == accept[i])
+	{
+		i++;
+	}
+
+	return (i);
+}
