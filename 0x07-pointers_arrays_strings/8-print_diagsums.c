7573b7e1f3b73c9fa8d757da3400ed4a73c65bdc
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-06-29 19:36:10 +0100

    j

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,13 +13,13 @@ void print_diagsums(int *a, int size)
 
 	n1 = 0;
 	n2 = 0;
-	for (i = 1; i < size; ++i)
+	for (i = 0; i < size; ++i)
 	{
-		n1 += a[i];
+		n1 += a[(size + 1) * i];
 	}
-	for (i = size - 1; i > 0; --i)
+	for (i = size - 1; i >= 0; --i)
 	{
-		n2 += a[i];
+		n2 += a[(size - 1) * (i + 1)];
 	}
 	printf("%ld, %ld\n", n1, n2);
 }
