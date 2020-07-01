061a784ca1135fedf108cae75e58052d9e60be8d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-01 13:23:56 +0100

    ok

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* no - the true square
+*@i: counter
+*@n: the square
+* Return: int
+*/
+int no(int n, int i)
+{
+if (i <= 0)
+{
+return (-1);
+}
+if (i * i == n)
+{
+return (i);
+}
+return (no(n, i - 1));
+}
+/**
+*_sqrt_recursion - the true square
+*@n: the square
+* Return: int
+*/
+int _sqrt_recursion(int n)
+{
+return (no(n, n));
+}
