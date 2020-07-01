3e560f64d6ae3d6731e8d84e03d5a63365677679
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-01 13:32:12 +0100

    ok

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,30 +3,32 @@
 *no - the true square
 *@i: counter
 *@n: the square
-* Return: int
+*Return: int
 */
 int no(int n, int i)
 {
-if (i <= 0)
-{
-return (-1);
-}
-if (i * i == n)
-{
-return (i);
-}
-return (no(n, i - 1));
+    if (i <= 0)
+    {
+        return (-1);
+    }
+
+    if (i * i == n)
+    {
+        return (i);
+    }
+    return (no(n, i - 1));
 }
 /**
 *_sqrt_recursion - the true square
 *@n: the square
-* Return: int
+*Return: int
 */
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-{
-return (0);
-}
-return (no(n, n));
+    if (n == 0)
+    {
+        return (0);
+    }
+
+    return (no(n, n));
 }
