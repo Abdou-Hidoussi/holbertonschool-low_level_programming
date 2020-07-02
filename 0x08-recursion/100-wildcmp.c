56ae5eb83d4b65572ccaedf5d41bda4401dc750b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-02 09:51:46 +0100

    fd

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -57,9 +57,5 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if ((s1[0] == '\0' && s2[0] != '\0') || (s1[0] != '\0' && s2[0] == '\0'))
-	{
-		return (0);
-	}
 	return (comp(s1, s2, 0, 0));
 }
