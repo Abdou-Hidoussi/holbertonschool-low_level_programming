a9fc134f208721d76aca3812908cc0f0f5066f78
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-04 18:38:27 +0100

    -.-

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_isalpha - 0
+*@c: integer
+*Return: either 1 or 0
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
