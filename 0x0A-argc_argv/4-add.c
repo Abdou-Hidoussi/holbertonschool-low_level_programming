bc9ef41b41d44b9e8b438ae02cb6a59f3143df0a
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-05 15:08:00 +0100

    first

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	r = 0;
 	while (i < argc)
 	{
-		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
 			r += atoi(argv[i]);
 			i++;
