5fd3f94dcbaffd61b944892cbfcd73c2fd605426
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-05 14:51:13 +0100

    first

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*main - 0
+*@argc: number of strings
+*@argv: number of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, r;
+
+	i = 1;
+	r = 0;
+	while (i < argc)
+	{
+		r += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", r);
+	return (0);
+}
