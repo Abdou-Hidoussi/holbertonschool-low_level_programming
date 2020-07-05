ea3796d32437c68eebf4637c5cddfb5e18d37fa5
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-05 16:24:51 +0100

    first

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		j = 0;
 		while (j < argv[i][j])
 		{
-			if (!(*argv[i][j] >= '0' && *argv[i][j] <= '9'))
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 			{
 				printf("Error\n");
 				return (1);
