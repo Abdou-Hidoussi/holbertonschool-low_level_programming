b64019079b0ed01a993fd93c335db4904c2726d4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-05 13:40:46 +0100

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - 0
+*@argc: number of strings
+*@argv: number of arguments
+*Return: 0
+*/
+int main(int argc, char const *argv[argc])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
