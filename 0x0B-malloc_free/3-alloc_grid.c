83b31f7989dfd7767e031596d8932c6d46cfb4c4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-07 15:00:21 +0100

    go

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,16 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	i = 0;
-	p = (int **)malloc(width * sizeof(int *));
+	p = malloc(width * sizeof(int *));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
+	i = 0;
 	while (i < height)
 	{
-		p[i] = (int *)malloc(height * sizeof(int));
+		j = 0;
+		p[i] = malloc(height * sizeof(int));
 		if (p[i] == NULL)
 		{
 			for (j = 0; j < i; ++j)
@@ -35,12 +37,6 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
-		i++;
-	}
-	i = 0;
-	while (i < height)
-	{
-		j = 0;
 		while (j < width)
 		{
 			p[i][j] = 0;
@@ -49,4 +45,56 @@ int **alloc_grid(int width, int height)
 		i++;
 	}
 	return (p);
+}
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
+{
+    int w;
+    int h;
+
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int **grid;
+
+    grid = alloc_grid(50, 50);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 50, 50);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 50, 50);
+    return (0);
 }
