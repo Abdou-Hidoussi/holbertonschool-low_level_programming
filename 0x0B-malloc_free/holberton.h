b75d560fde89b80437cf3bd1e024c5882162bdc7
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-07 12:16:07 +0100

    fix null

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-
+void simple_print_buffer(char *buffer, unsigned int size);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
