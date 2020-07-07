b718aa76b2b5c5a110a3daa5158940cd891e2667
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-07 15:29:31 +0100

    plz

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,13 +35,17 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
 		j = 0;
 		while (j < width)
 		{
 			p[i][j] = 0;
 			j++;
 		}
-		i++;
 		i++;
 	}
 	return (p);
