b9f413a7ca449ac094d161de2c65b76e7dee56e5
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-07 12:28:54 +0100

    fix conditions

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	r = 0;
-	while (s1[i] != NULL)
+	while (s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2[j] != NULL)
+	while (s2[j] != '\0')
 	{
 		j++;
 	}
