3748b0508db4cc72526263b360df369605b20574
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-08 08:59:24 +0100

    memory fix ?

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,14 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	i = 0;
-	p = malloc(width * sizeof(p));
+	p = malloc(width * sizeof(int *));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 	while (i < height)
 	{
-		p[i] = malloc(height * sizeof(int *));
+		p[i] = malloc(height * sizeof(int));
 		if (p[i] == NULL)
 		{
 			for (j = 0; j < i; ++j)
