9f7eaaa93c05edf95751df241e550521ce52fbb9
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-09 13:13:30 +0100

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,17 +13,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 	unsigned int i, j, t, r;
 
-	for (i = 0; s1[i] != '\0' ; ++i)
-		;
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0' ; ++i)
+			;
+	}
+	else
+		i = 0;
 	if (s2 != NULL)
 	{
 		for (j = 0; s2[j] != '\0' ; ++j)
 			;
 	}
 	else
-	{
 		r = 0;
-	}
 	if (n >= j)
 	{
 		p = malloc((i + j) * sizeof(char));
@@ -35,20 +38,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		r = n;
 	}
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	for (t = 0; t < (i + r); ++t)
 	{
-		if (t <= i - 1)
-		{
+		if (t < i)
 			p[t] = s1[t];
-		}
 		else
-		{
 			p[t] = s2[t - i];
-		}
 	}
-	p[t + 1] = '\0';
 	return (p);
 }
