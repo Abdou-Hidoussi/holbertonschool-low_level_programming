9d97228958b40c758117bf97e60ebca616966df8
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-09 15:23:11 +0100

    a

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - 0
+ * @nmemb: n element
+ * @size: size element
+ * Return: array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i, j;
+	int **p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * sizeof(int *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; ++i)
+	{
+		p[i] = malloc(size);
+		if (p[i] == NULL)
+		{
+			for (j = 0; j <= i; ++j)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < nmemb; ++i)
+	{
+		p[i] = 0;
+	}
+
+	return (p);
+
+}
