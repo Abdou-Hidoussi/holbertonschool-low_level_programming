45628450867ae19bbda68da59b930fc6e0b7e3c8
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-13 16:34:35 +0100

    doggo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_FILE
 #define DOG_FILE
-
-struct dog {
-	char* name;
+/**
+*struct dog - dog
+*@name: name
+*@age: float
+*@owner: owner
+*/
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
-} dog;  
+	char *owner;
+} dog;
 
 #endif
