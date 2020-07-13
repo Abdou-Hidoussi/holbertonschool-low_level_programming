4faf21423409ea185077e1d6bd0fec04aee49a82
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-13 18:55:01 +0100

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,18 +19,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; name[i] != '\n'; ++i)
 	(*d).name = malloc(sizeof(i * (char) +1));
 	if ((*d).name  == NULL)
+	{
 		free((*d).name);
 		free(d);
-		return (NULL);
+		return (NULL);*
+	}
 	if ((*d).name)
 		for (i = 0; name[i] != '\0'; ++i)
 			(*d).name[i] = name[i];
 	for (i = 0; name[i] != '\n'; ++i)
 	(*d).owner = malloc(sizeof(i * (char) +1));
 	if ((*d).owner  == NULL)
+	{
 		free((*d).owner);
 		free(d);
 		return (NULL);
+	}
 	if ((*d).owner)
 		for (i = 0; owner[i] != '\0'; ++i)
 			(*d).owner[i] = owner[i];
