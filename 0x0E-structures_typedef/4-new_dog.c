596ea06f79de28fea275f2dff8fe13fdba20366c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-13 18:55:44 +0100

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free((*d).name);
 		free(d);
-		return (NULL);*
+		return (NULL);
 	}
 	if ((*d).name)
 		for (i = 0; name[i] != '\0'; ++i)
