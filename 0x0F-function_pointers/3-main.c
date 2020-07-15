9b6b6bd25f2cd68ca8fa4bb3b31d19676c43eb9d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-15 14:32:52 +0100

    finally

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,17 +9,10 @@
 */
 int main(int argc, char *argv[])
 {
-	int (*f)(int a, int b);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
-	}
-	f = get_op_func(argv[2]);
-	if (f == NULL)
-	{
-		exit(99);
 	}
 	printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
