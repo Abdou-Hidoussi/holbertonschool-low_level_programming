706ad543ed7ead7d476d3b26453e5ea18c48deda
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-15 02:55:51 +0100

    -.-

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+*array_iterator - 0
+*@array: array
+*@size: size of array
+*@action: a function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; ++i)
+	{
+		(*action)(array[i]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+*int_index - 0
+*@array: array
+*@size: size of array
+*@cmp: a function
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if((*cmp)(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
