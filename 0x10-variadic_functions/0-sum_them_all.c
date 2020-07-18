a3771528c39d7bca87894b172f34001a98f76d29
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-18 12:42:34 +0100

    ?

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0
+	int sum = 0;
 	unsigned int i;
 	va_list ap;
 
@@ -21,4 +21,22 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(ap);
 	return (sum);
+}
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int sum;
+
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);    
+    return (0);
 }
