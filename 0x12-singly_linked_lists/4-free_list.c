55b011fae5bcef8a4a9478580c4f8808cf8c835b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-23 21:15:13 +0100

    5

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_list - 0
+*
+*@h: list_t
+*/
+void free_list(list_t *h)
+{
+	struct list_s *next = (*h).next;
+
+	while (next != NULL)
+	{
+		next = (*h).next;
+		free((*h).str);
+		free(h);
+		h = next;
+	}
+}
