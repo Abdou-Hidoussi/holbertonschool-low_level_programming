43e42a68484f2d880e830588a822c3c77496fd42
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-23 19:07:24 +0100

    d

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,24 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+*_strlen - 0
+*@s: char
+*Return:: i
+*/
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0' && s)
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+/**
 *add_node_end - 0
 *
 *@head: list_t
