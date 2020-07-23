13547a4ed4560e659a73841f1d9dacde709a13b0
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-23 16:37:44 +0100

    c1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,9 +12,9 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new;
 
-
+	new = malloc(sizeof(list_t));
 	(*new).str = malloc(sizeof(str));
 	(*new).str = strdup(str);
 	(*new).len = strlen(str);
