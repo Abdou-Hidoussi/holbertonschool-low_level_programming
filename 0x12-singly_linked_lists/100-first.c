14a1f7aedeffd61bd24f28ac942d2786808b65ac
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-23 23:21:25 +0100

    a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
 /**
 *tortuga - will be called before main
-*
-*
-*
 */
 void tortuga(void) __attribute__ ((constructor));
+
 void tortuga(void)
 {
 	printf("You're beat! and yet, you must allow,\n
