ab14948a6d408b0f4ba7e251e4441ab16f9d263d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-28 19:20:43 +0100

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,5 +38,5 @@ listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n)
 	(*next).next = (*head).next;
 	(*head).next = next;
 	(*next).n = t;
-	return ((*head).next);
+	return (next);
 }
