fb886cab4aa0299ab8d18b30cc5ccbfa91b83e23
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-28 16:29:13 +0100

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_listint2 - 0
+*
+*@head: listint_t
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *next, *h;
+
+	h = *head;
+	if (h != NULL)
+	{
+		while (h != NULL)
+		{
+			next = (*h).next;
+			free(h);
+			if (next == NULL)
+				h = NULL;
+			else
+				h = next;
+		}
+		free(h);
+		free(next);
+	}
+}
