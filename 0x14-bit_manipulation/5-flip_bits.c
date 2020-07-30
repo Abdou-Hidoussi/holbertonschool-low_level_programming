4d01f334d1b1073b8d2e9c2783d8e94c3bbfbb0f
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-30 20:07:21 +0100

    d

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*flip_bits - 0
+*@n: number
+*@m: number
+*Return: i
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = 0;
+
+	while(!n && !m)
+	{
+		if ((n & 1) != (m & 1))
+		{
+			i++;
+		}
+		n >>= 1;
+		m >> = 1;
+	}
+	return (i);
+}
