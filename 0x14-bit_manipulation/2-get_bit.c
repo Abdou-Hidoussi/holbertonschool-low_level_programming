81aa7b75c8228934f7c09a473785f54730a0c150
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-30 19:27:54 +0100

    stupidity

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,17 +7,12 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-{
-	unsigned int i = 0;
-
-	if (n == 0 && index < 64)
-		return (0);
-	while (i <= 63)
+	if (index < 64)
 	{
-		if (index == i)
-			return (n & 1);
-		n >>= 1;
-		i++;
+		return ((n >> index) & 1);
 	}
-	return (-1);
+	else
+	{
+		return (-1);
+	}
 }
