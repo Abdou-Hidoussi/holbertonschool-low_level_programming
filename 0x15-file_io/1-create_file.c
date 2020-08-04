2b64fc0fd8fcddabb5d257f5040c5450315338d4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-08-04 15:52:27 +0100

    a

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*create_file - 0
+*@filename: filename
+*@text_content: content
+*Return: int
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+	unsigned long int a;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (!file)
+		return (-1);
+	for (i = 0; text_content[i]; ++i)
+		;
+	a = write(file, text_content, i);
+	if (!a)
+		return (-1);
+	return (1);
+}
