6c3568a76dd5951cd3faaaacafdecb928f4356bc
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-08-04 16:09:01 +0100

    b

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,10 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	for (i = 0; text_content[i]; ++i)
-		;
 	if (text_content)
 	{
+		for (i = 0; text_content[i]; ++i)
+			;
 		a = write(file, text_content, i);
 		if (a == -1)
 			return (-1);
