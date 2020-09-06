b71316b361ac28f9cee34b6a7a10e1ef26ac132c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-06 17:39:47 +0100

    fix position

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new)
 	{
 		(*new).n = n;
-		(*new).next = *head;
+		(*new).next = NULL;
 		(*new).prev = NULL;
+		if (head != NULL)
+		{
+			(*new).next = *head;
+			new = (*new).next;
+			(*new).prev = new;
+		}
 		*head = new;
 		return (new);
 	}
