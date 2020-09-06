27aaea2bc65945dd198037de3154086392a43c40
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-06 17:43:10 +0100

    add the prev to the head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, *h;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new)
@@ -20,6 +20,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*new).n = n;
 		(*new).next = *head;
 		(*new).prev = NULL;
+		h = *head;
+		(*h).prev = new;
 		*head = new;
 		return (new);
 	}
