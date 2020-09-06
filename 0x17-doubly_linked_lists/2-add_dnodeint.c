06dcfe8459970c99ede694ccdc2a5ca0f10925eb
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-06 17:41:35 +0100

    revert

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,14 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new)
 	{
 		(*new).n = n;
-		(*new).next = NULL;
+		(*new).next = *head;
 		(*new).prev = NULL;
-		if (head != NULL)
-		{
-			(*new).next = *head;
-			new = (*new).next;
-			(*new).prev = new;
-		}
 		*head = new;
 		return (new);
 	}
