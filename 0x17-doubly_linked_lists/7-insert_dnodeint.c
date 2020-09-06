2056eda6b1dff6abf0e06752954ddf1baf45f80c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-06 16:21:29 +0100

    initial

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+*dlistint_len - 0
+*
+*@h: dlistint_t
+*
+*Return: int
+*
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int  i = 0;
+
+	while (h != NULL)
+	{
+		h = (*h).next;
+		i++;
+	}
+	return (i);
+}
+/**
+*insert_dnodeint_at_index - 0
+*
+*@head: dlistint_t
+*
+*@idx: int
+*
+*@n: int
+*
+*Return: dlistint_t
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
+	unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new, *h, *a;
+
+	h = *head;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new = add_dnodeint(&h, n);
+	}
+	if (idx == dlistint_len(h))
+	{
+		new = add_dnodeint_end(&h, n);
+	}
+	while (h)
+	{
+		if (i == idx)
+		{
+			(*new).prev = h;
+			(*new).next = (*h).next;
+			(*new).n = n;
+			a = (*h).next;
+			(*a).prev = new;
+			(*h).next = new;
+			return (new);
+		}
+		i++;
+		h = (*h).next;
+	}
+	return (new);
+}
