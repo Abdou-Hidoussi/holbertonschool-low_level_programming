30d20774a89ac50410683edcc1c8c92e9f1c2b24
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-09-06 15:21:16 +0100

    initial

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_dlistint - 0
+*
+*@h: dlistint_t
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	if (h != NULL)
+	{
+		while (h != NULL)
+		{
+			next = (*h).next;
+			free(h);
+			if (next == NULL)
+				h = NULL;
+			else
+				h = next;
+		}
+		free(h);
+		free(next);
+	}
+}
