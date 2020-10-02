5285c42487fb9e3000aec41bf0a709dacf3ef0b4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-02 19:31:28 +0000

    initial

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (tab == NULL)
 		return (NULL);
 
-	if (size == 0)
-		return (tab);
-
 	(*tab).array = malloc(sizeof(hash_node_t *) * size);
 	if ((*tab).array == NULL)
 		return (NULL);
